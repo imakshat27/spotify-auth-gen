a4306e2a0452bb78cdaeaa27dd3e33048b7573a4
Akshat <agarwalakshat2710@gmail.com>
2025-11-27 23:26:33 +0530

    fix: update redirect URI to correct endpoint for Spotify callback

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,6 +1,6 @@
 const clientId = process.env.SPOTIFY_CLIENT_ID;
-const encodedRedirectUri = "https%3A%2F%2Fspotify-auth-gen.vercel.app%2Ftoken";
-export const redirectUri = "https://spotify-auth-gen.vercel.app/token";
+export const redirectUri = "https://spotify-auth-gen.vercel.app/api/spotify/callback";
+const encodedRedirectUri = encodeURIComponent(redirectUri);
 const scopes = "user-read-currently-playing";
 
 const authUri = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=code&redirect_uri=${encodedRedirectUri}&scope=${scopes}`;
